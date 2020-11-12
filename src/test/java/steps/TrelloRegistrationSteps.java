a6747a1fcb2aff43668c890b68df703e0273f67b
munichbughunter <munichbughunter@gmail.com>
2020-11-12 08:43:09 +0100

    Initial commit

---
+++ src/test/java/steps/TrelloRegistrationSteps.java
@@ -0,0 +1,27 @@
+package steps;
+
+import io.cucumber.java8.En;
+
+
+
+public class TrelloRegistrationSteps implements En {
+
+    //TrelloPage trelloPage = null;
+
+    public TrelloRegistrationSteps() {
+
+        Given("^I am on Trello page$", () -> {
+      //      trelloPage = open(TrelloPage::new);
+        });
+
+        When("^I click on registration button$", () -> {
+        //    trelloPage.clickRegistrationButton();
+        });
+
+        Then("^Registration page opened$", () -> {
+
+        });
+
+
+    }
+}
